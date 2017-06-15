d2122c4150e0fc1d0deb8f33e3a67e56a82a76b4
Bhagat Singh <bhagatsingh2297@gmail.com>
2017-06-16 01:32:51 +0530

    first commit

--- ShakeMe/ViewController.swift
+++ ShakeMe/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: UIWebView!
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do{
+            let url = Bundle.main.url(forResource: "batman_on_drugs", withExtension: "mp3")!
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.prepareToPlay()
+        }catch{
+            print("Audio Error \(error)")
+        }
+        
+        webView.isHidden = true
+        let url = Bundle.main.url(forResource: "batman", withExtension: "gif")!
+        let data = try! Data(contentsOf: url)
+        webView.load(data, mimeType: "image/gif", textEncodingName: "UTF-8", baseURL: NSURL() as URL)
+        webView.scalesPageToFit = true
+        webView.contentMode = UIViewContentMode.scaleAspectFit
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+            webView.isHidden = false
+            audioPlayer.play()
+            print("Started")
+        }
     }
 
-
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+            webView.isHidden = true
+            audioPlayer.pause()
+            
+            print("Ended")
+        }
+    }
 }
-
